0a8a936d0f0154b8e8750ed30f43f4d976a01c83
unknown <sangjy22@gmail.com>
2022-11-16 21:51:06 +0900

    vue version2

--- skeleton/src/App.vue
+++ skeleton/src/App.vue
@@ -1,63 +1,89 @@
 <template>
   <div id="app">
-    <div class="facebook-card">
-      <vue-skeleton-loader
-        type="circle"
-        :width="50"
-        :height="50"
-        animation="fade"
-      />
-    </div>
-    <div class="facebook-card">
-      <vue-skeleton-loader
-        type="rect"
-        :width="200"
-        :height="20"
-        animation="wave"
-      />
-    </div>
-    <div class="facebook-card">
-      <vue-skeleton-loader
-        type="circle"
-        :width="20"
-        :height="20"
-        animation="wave"
-      />
-    </div>
-    <div class="facebook-card">
-      <vue-skeleton-loader
-        type="rect"
-        color="tomato"
-        :rounded="true"
-        :width="300"
-        :height="20"
-        animation="wave"
-      />
-    </div>
-    <div class="facebook-card">
-      <vue-skeleton-loader
-        type="circle"
-        wave-color="red"
-        :rounded="true"
-        :width="50"
-        :height="50"
-        animation="wave"
-      />
-    </div>
-    <div class="facebook-card">
-      <vue-skeleton-loader
-        type="rect"
-        :rounded="true"
-        :width="300"
-        :height="50"
-        animation="pulse"
-      />
-    </div>
+    <h1>Skeleton</h1>
+    <table border="1" width="500px">
+      <th>circle</th>
+      <th>rect</th>
+      <tr>
+        <td>
+          <div class="facebook-card">
+            <vue-skeleton-loader
+              type="circle"
+              :width="50"
+              :height="50"
+              animation="fade"
+            />
+          </div>
+        </td>
+        <td>
+          <div class="facebook-card">
+            <vue-skeleton-loader
+              type="rect"
+              :width="200"
+              :height="20"
+              animation="wave"
+            />
+          </div>
+        </td>
+      </tr>
+      <tr>
+        <td>
+          <div class="facebook-card">
+            <vue-skeleton-loader
+              type="circle"
+              :width="20"
+              :height="20"
+              animation="wave"
+            />
+          </div>
+        </td>
+        <td>
+          <div class="facebook-card">
+            <vue-skeleton-loader
+              type="rect"
+              color="tomato"
+              :rounded="true"
+              :width="300"
+              :height="20"
+              animation="wave"
+            />
+          </div>
+        </td>
+      </tr>
+      <tr>
+        <td>
+          <div class="facebook-card">
+            <vue-skeleton-loader
+              type="circle"
+              wave-color="red"
+              :rounded="true"
+              :width="50"
+              :height="50"
+              animation="wave"
+            />
+          </div>
+        </td>
+        <td>
+          <div class="facebook-card">
+            <vue-skeleton-loader
+              type="rect"
+              :rounded="true"
+              :width="300"
+              :height="50"
+              animation="pulse"
+            />
+          </div>
+        </td>
+      </tr>
+      <tr>
+        <td>component</td>
+        <td></td>
+      </tr>
+      </table>
   </div>
 </template>
 
 <script>
-
 export default {
   name: 'App',
   components: {
